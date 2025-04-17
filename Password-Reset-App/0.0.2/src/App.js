57a1f8f260f9a899c27dd88bf97d758670d986e8
shivam-dubey-byte <dubeyshivamraj@gmail.com>
2025-04-18 00:40:13 +0530

    0.0.3

--- Password-Reset-App/0.0.2/src/App.js
+++ Password-Reset-App/0.0.2/src/App.js
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResetPassword from "./ResetPassword";
 
+// Define a Home component for the root route
+function Home() {
+  return <h1>Welcome to the Home Page!</h1>;
+}
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Route for the home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Route for reset password with token */}
         <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
     </Router>
